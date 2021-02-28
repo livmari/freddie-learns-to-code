9c6fe2075ab1ced489fc90b99fc0faf5cd9ca0eb
livm <livm@student.chalmers.se>
2021-02-28 16:34:24 +0100

    Todo comments for next steps in the code

--- palindrome-checker/app.js
+++ palindrome-checker/app.js
@@ -4,5 +4,9 @@ document.addEventListener("DOMContentLoaded", event => {
     // will display palindrome result in DOM
     let resultContainer =document.getElementById("result")
 
-    
+    // TODO: handle form data to get input word
+
+    // TODO: create if-statement to check if word is a palindrome
+
+    // TODO: update resultContainer to indicate to the user if the word is a palindrome
 })
